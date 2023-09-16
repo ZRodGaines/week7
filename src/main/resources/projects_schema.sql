a5d7064912a5e7b6391b597e0414a3d0ba32016c
ZrodGaines <ZRodGaines@gmail.com>
2023-09-16 08:28:37 -0400

    second commit

---
+++ src/main/resources/projects_schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXSISTS project_category;
+DROP TABLE IF EXSISTS step;
+DROP TABLE IF EXSISTS material;
+DROP TABLE IF EXSISTS category;
+DROP TABLE IF EXSISTS project;
+
+CREATE TABLE project(
+
+);
+
+CREATE TABLE category();
+
+CREATE TABLE material();
+
+CREATE TABLE step();
+
+CREATE TABLE project_category();
